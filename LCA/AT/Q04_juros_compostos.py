c06b4ead8f5bf8cf6535a53efd2731adc1fe04c3
Rafael Lima <rafaellimagms@gmail.com>
2020-09-23 11:22:29 -0300

    Redefinição de chamada de funções

--- LCA/AT/Q04_juros_compostos.py
+++ LCA/AT/Q04_juros_compostos.py
@@ -4,16 +4,20 @@ def get_entry_data():
     monthly_inv = float(input('Aporte mensal: '))
     terms = int(input('Período de aplicação (meses): '))
 
-    final_balance(initial_inv, monthly_prof, monthly_inv, terms)
+    calc_final_balance(initial_inv, monthly_prof, monthly_inv, terms)
 
 
-def final_balance(initial_inv, monthly_prof, monthly_inv, terms):
+def calc_final_balance(initial_inv, monthly_prof, monthly_inv, terms):
     accumulated_prof = initial_inv
     for mes in range(terms):
-        accumulated_prof = monthly_inv + (accumulated_prof * (1 + monthly_prof))
+        accumulated_prof = monthly_inv + \
+            (accumulated_prof * (1 + monthly_prof))
 
+    show_final_balance(accumulated_prof)
+
+
+def show_final_balance(accumulated_prof):
     print(f"{accumulated_prof:.2f}")
 
 
 get_entry_data()
-
