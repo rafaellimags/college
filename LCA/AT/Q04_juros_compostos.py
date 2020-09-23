a6e9e162f3a45ff7f717d436e8d7a0cc62dccd90
Rafael Lima <rafaellimagms@gmail.com>
2020-09-23 14:50:13 -0300

    Plotagem do gráfico da evolução patrimonial

--- LCA/AT/Q04_juros_compostos.py
+++ LCA/AT/Q04_juros_compostos.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def get_entry_data():
     initial_inv = float(input('Valor inicial: '))
     monthly_prof = float(input('Rendimento (a.m.): ')) / 100
@@ -9,15 +13,27 @@ def get_entry_data():
 
 def calc_final_balance(initial_inv, monthly_prof, monthly_inv, terms):
     accumulated_prof = initial_inv
+    months = []
+    balance = []
+
     for mes in range(terms):
+        months.append(mes)
+        balance.append(accumulated_prof)
         accumulated_prof = monthly_inv + \
             (accumulated_prof * (1 + monthly_prof))
 
-    show_final_balance(accumulated_prof)
+    show_final_balance(accumulated_prof, months, balance)
 
 
-def show_final_balance(accumulated_prof):
-    print(f"{accumulated_prof:.2f}")
+def show_final_balance(accumulated_prof, months, balance):
+    plt.plot(months, balance)
+    plt.title('Evolução patrimonial')
+    plt.xlabel('Meses')
+    plt.ylabel('Saldo (R$)')
+    plt.grid(True)
+    plt.show()
+    print(f'Saldo final: R$ {accumulated_prof:.2f}')
+    print(f'Período total da aplicação: {len(months)} meses')
 
 
 get_entry_data()
