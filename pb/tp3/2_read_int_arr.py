d1ae9820711cd97b0b0fde4dbfb0c4b075f0f424
Rafael Lima <rafaellimagms@gmail.com>
2020-11-20 16:37:15 -0300

    Tp3

--- pb/tp3/2_read_int_arr.py
+++
@@ -1,9 +0,0 @@
-int_arr = []
-
-while True:
-    num_in = int(input(f'Números inteiros: '))
-    int_arr.append(num_in)
-    print(int_arr)
-    if len(int_arr) == 4:
-        print('Fim da execução\n')
-        break
--- pb/tp3/3_revert_string.py
+++
@@ -1,9 +0,0 @@
-words = []
-
-for i in range(10):
-    number = str(i)
-    words.append('palavra_' + number)
-
-words.reverse()
-
-print(words)
